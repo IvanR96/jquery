157ff97724ff3199ccafffc050896c6aa723c405
IvanR96 <irios96@hotmail.com>
2024-07-02 22:42:29 -0700

    event listener jQuery done

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 $('h1').text("Welcome to jQuery!");
 
 $("button").text("Click Here");
+
+$("a").attr("href", "https://www.youtube.com");
+
+$("button").click(function (){
+    $("h1").css("color", "blue");
+});
