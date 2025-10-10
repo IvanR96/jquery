b7f394898c58da4266704b4b5ee4a54681db96f8
IvanR96 <irios96@hotmail.com>
2025-10-10 01:14:58 -0700

    events added

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 $("h1").addClass("big-title");
 
-$("button").html("<em>Hey</em>");
-
 $("img").attr("src");
 
 $("a").attr("href", "https://www.yahoo.com");
 
+$("h1").click(function (){
+   $("h1").css("color", "purple"); 
+});
 
+$("button").click(function (){
+    $("h1").css("color", "purple"); 
+})
 
+$("input").keypress(function(event){
+    $("h1").html(event.key);
+});
+
+$("h1").on("mouseover", function(){
+    $("h1").css("color", "blue");
+});
